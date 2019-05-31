74d36df0736cda7a0199b77cddf5d9b537f4d9b5
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-31 16:03:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,8 +26,10 @@ end
 
 def printer(attendees)
   list = []
-  attendees.each do
-    list << batch_badge_creator(attendees)
-    list << assign_rooms(attendees)
+  batch_badge_creator(attendees)
+  assign_rooms(attendees)
+  attendees.each do |badges, room_assignments|
+    list << badges + "," room_assignments
   end
+  puts list
 end
