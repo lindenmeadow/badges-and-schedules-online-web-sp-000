b415d940dd0a8394f8e00c22f0a7db4b1bf9e28d
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-31 15:43:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,6 +25,6 @@ def assign_rooms(attendees)
 end
 
 def printer(badges, room_assignments)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  puts badges
+  puts room_assignments
 end
