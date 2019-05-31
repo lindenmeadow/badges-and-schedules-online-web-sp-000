d158184fbade5b7418670e696082d3158a1f904b
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-31 15:14:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,11 @@ end
 
 
 def assign_rooms(attendees)
-
+  room = 1
+  attendees.each do |name|
+    puts "Hello, #{name}! You'll be assigned to room #{room}."
+  	room += 1
+  end
 end
 
 def printer
